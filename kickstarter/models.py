d2331ea56039353eb42d2aca27d4cdade4110dbf
Banhidy Krisztian <admin@szamuraj.com>
2016-07-14 10:11:04 +0200

    Nightly update

--- kickstarter/models.py
+++ kickstarter/models.py
@@ -1,29 +1,50 @@
 from kickstarter import db
+from sqlalchemy.sql import func
+import datetime
 
 
-class Member(db.model):
-    id = db.Column(db.Integer,primary_key=True)
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     project = db.relationship('Project', backref='creator')
-    pledges = db.relationship('Pledge', backref='pledger',foreign_key='Pledge.member_id')
+    pledges = db.relationship('Pledge', backref='pledger', foreign_keys='Pledge.member_id')
 
-class Project(db.model):
-    id = db.Column(db.Integer,primary_key=True)
-    member_id = db.Column(db.Integer,db.ForeignKey('member.id'),nullable=False)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
     name = db.Column(db.String(100))
-    short_descriptin = db.Column(db.Text)
-    long_descriptin = db.Column(db.Text)
+    short_description = db.Column(db.Text)
+    long_description = db.Column(db.Text)
     goal_amount = db.Column(db.Integer)
     time_start = db.Column(db.DateTime)
     time_end = db.Column(db.DateTime)
     time_created = db.Column(db.DateTime)
-    pledges = db.relationship('Pledge', backref='project',foreign_key='Pledge.project_id')
+    pledges = db.relationship('Pledge', backref='project', foreign_keys='Pledge.project_id')
+
+    @property
+    def num_pledges(self):
+        return len(self.pledges)
+
+    @property
+    def total_pledges(self):
+        total_pledges = db.session.query(func.sum(Pledge.amount)).filter(Pledge.project_id == self.id).one()[0]
+        if total_pledges is None:
+            total_pledges = 0
+        return total_pledges
+
+    @property
+    def num_days_left(self):
+        now = datetime.datetime.now()
+        num_days_left = (self.time_end - now).days
+
+        return num_days_left
 
 
-class Pledge(db.model):
-    id = db.Column(db.Integer,primary_key=True)
-    member_id = db.Column(db.Integer,db.ForeignKey('member.id'),nullable=False)
-    project_id = db.Column(db.Integer,db.ForeignKey('project.id'),nullable=False)
+class Pledge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
     amount = db.Column(db.Integer)
     time_created = db.Column(db.DateTime)
--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
 from kickstarter import manager
 
+# import pydevd
+
 
 if __name__ == '__main__':
+    #    pydevd.settrace( '10.10.16.70', port =  21000)
     manager.run()
