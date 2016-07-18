c47563ee07167d3948366f04e7d0181320b3b8d5
Banhidy Krisztian <admin@szamuraj.com>
2016-07-18 22:31:31 +0200

    Graph and other features

--- kickstarter/__init__.py
+++ kickstarter/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, flash
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore
+from flask_security import Security, SQLAlchemyUserDatastore, login_required, current_user
 import datetime
 from cloudinary import uploader
 
@@ -13,6 +13,7 @@ manager = Manager(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+migrate.init_app(app, db, render_as_batch=True)
 manager.add_command('db', MigrateCommand)
 
 from kickstarter.models import *
@@ -22,6 +23,8 @@ user_datastore = SQLAlchemyUserDatastore(db, Member, Role)
 security = Security(app, user_datastore, register_form=ExtendedRegisterForm)
 
 mail = Mail(app)
+
+
 @app.route('/')
 def hello():
     projects = db.session.query(Project).order_by(Project.time_created.desc()).limit(15)
@@ -29,6 +32,7 @@ def hello():
 
 
 @app.route('/projects/create/', methods=['GET', 'POST'])
+@login_required
 def create():
     if request.method == "GET":
         return render_template("create.html")
@@ -41,7 +45,7 @@ def create():
         cover_photo = request.files['cover_photo']
         uploaded_image = cloudinary.uploader.upload(cover_photo, crop="limit", width=680, height=550)
         image_filename = uploaded_image["public_id"]
-        new_project = Project(member_id=1, name=request.form.get("project_name"),
+        new_project = Project(member_id=current_user.id, name=request.form.get("project_name"),
                               short_description=request.form.get("short_description"),
                               long_description=request.form.get("long_description"),
                               goal_amount=request.form.get("funding_goal"), time_start=now, time_end=time_end,
@@ -50,7 +54,7 @@ def create():
         db.session.add(new_project)
         db.session.commit()
 
-        return redirect(url_for('project_detail', project_id=new_project.id))
+        return redirect(url_for('create_rewards', project_id=new_project.id))
 
 
 @app.route("/projects/<int:project_id>/")
@@ -61,7 +65,31 @@ def project_detail(project_id):
     return render_template("project_detail.html", project=project)
 
 
+@app.route("/projects/<int:project_id>/rewards/", methods=["GET", "POST"])
+@login_required
+def create_rewards(project_id):
+    project_query = db.session.query(Project).filter(Project.member_id == current_user.id, Project.id == project_id)
+    if project_query.count() == 0:
+        abort(404)
+    project = project_query.one()
+    if request.method == "GET":
+        return render_template("create_rewards.html", project=project)
+    elif request.method == "POST":
+        titles = request.form.getlist('title[]')
+        min_pledges = request.form.getlist('min_pledge[]')
+        descriptions = request.form.getlist('description[]')
+
+        for i in range(5):
+            if titles[i] and descriptions[i] and min_pledges[i]:
+                new_reward = Reward(project_id=project.id, title=titles[i], description=descriptions[i],
+                                    minimum_pledge_amount=int(min_pledges[i]))
+            db.session.add(new_reward)
+        db.session.commit()
+        return redirect(url_for('project_detail', project_id=project.id))
+
+
 @app.route("/projects/<int:project_id>/pledge/", methods=["GET", "POST"])
+@login_required
 def pledge(project_id):
     project = db.session.query(Project).get(project_id)
     if project is None:
@@ -69,17 +97,45 @@ def pledge(project_id):
     if request.method == "GET":
         return render_template("pledge.html", project=project)
     elif request.method == "POST":
-        guest_pledgor = db.session.query(Member).filter_by(id=2).one()
+        amount = int(request.form.get("amount") or 0)
+        reward_id = request.form.get("reward_id")
+
+        if reward_id == "none":
+            reward_id = None
+        else:
+            reward_query = db.session.query(Reward).filter(Reward.project_id == project.id, Reward.id == reward_id)
+            if reward_query.count() == 0:
+                flash('Please choose a pledge reward')
+                return redirect(url_for('pledge', project_id=project.id))
+            reward = reward_query.one()
+
+            if amount < reward.minimum_pledge_amount:
+                flash('You must pledge at least $%s for that reward' % reward.minimum_pledge_amount)
+                return redirect(url_for('pledge', project_id=project.id))
+            if amount < 1:
+                flash("You must pledge at least $1")
+                return redirect(url_for('pledge', project_id=project.id))
+
         new_pledge = Pledge(
-            member_id=guest_pledgor.id,
+            member_id=current_user.id,
             project_id=project_id,
-            amount=request.form.get("amount"),
+            reward_id=reward_id,
+            amount=amount,
             time_created=datetime.datetime.now()
 
         )
         db.session.add(new_pledge)
         db.session.commit()
         return redirect(url_for('project_detail', project_id=project_id))
+
+
+@app.route('/projects/<int:project_id>/stats/')
+def stats(project_id):
+    project = db.session.query(Project).get(project_id)
+    if project is None:
+        abort(404)
+
+    return render_template('stats.html', project=project)
 
 
 @app.route('/search/')
--- kickstarter/models.py
+++ kickstarter/models.py
@@ -1,28 +1,30 @@
 from kickstarter import db, app
-from flask.ext.security import RoleMixin, UserMixin
 from sqlalchemy.sql import func
+from flask.ext.security import RoleMixin, UserMixin
 import datetime
 import cloudinary.utils
 
-roles_members = db.Table('roles_members', db.Column('member_id', db.INTEGER(), db.ForeignKey('member.id')),
-                         db.Column('role_id', db.INTEGER(), db.ForeignKey('role.id')))
+roles_members = db.Table('roles_members',
+                         db.Column('member_id', db.Integer(), db.ForeignKey('member.id')),
+                         db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 
 class Member(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100), nullable=False)
-    last_name = db.Column(db.String(100), nullable=False)
+    first_name = db.Column(db.String(64), nullable=False)
+    last_name = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
-    active = db.Column(db.String(255))
-    confirmed_at = db.Column(db.DATETIME())
-    project = db.relationship('Project', backref='creator')
-    pledges = db.relationship('Pledge', backref='pledger', foreign_keys='Pledge.member_id')
-    roles = db.relationship('Role', secondary=roles_members, backref=db.backref('members', lazy='dynamic'))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    project = db.relationship('Project', backref='creator', lazy='dynamic')
+    pledges = db.relationship('Pledge', backref='member', lazy='dynamic', foreign_keys='Pledge.member_id')
+    roles = db.relationship('Role', secondary=roles_members,
+                            backref=db.backref('members', lazy='dynamic'))
 
 
 class Role(db.Model, RoleMixin):
-    id = db.Column(db.INTEGER(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
@@ -30,26 +32,28 @@ class Role(db.Model, RoleMixin):
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
-    name = db.Column(db.String(100))
-    image_filename = db.Column(db.String(200))
-    short_description = db.Column(db.Text)
-    long_description = db.Column(db.Text)
-    goal_amount = db.Column(db.Integer)
-    time_start = db.Column(db.DateTime)
-    time_end = db.Column(db.DateTime)
-    time_created = db.Column(db.DateTime)
-    pledges = db.relationship('Pledge', backref='project', foreign_keys='Pledge.project_id')
-
-    @property
-    def num_pledges(self):
-        return len(self.pledges)
+    name = db.Column(db.String(64), nullable=False)
+    short_description = db.Column(db.Text, nullable=False)
+    long_description = db.Column(db.Text, nullable=False)
+    goal_amount = db.Column(db.Integer, nullable=False)
+    image_filename = db.Column(db.String(128), nullable=False)
+    time_created = db.Column(db.DateTime(timezone=False), nullable=False)
+    time_start = db.Column(db.DateTime(timezone=False), nullable=False)
+    time_end = db.Column(db.DateTime(timezone=False), nullable=False)
+    pledges = db.relationship('Pledge', backref='project', lazy='dynamic', foreign_keys='Pledge.project_id')
+    rewards = db.relationship('Reward', backref='project', lazy='dynamic', foreign_keys='Reward.project_id')
 
     @property
     def total_pledges(self):
         total_pledges = db.session.query(func.sum(Pledge.amount)).filter(Pledge.project_id == self.id).one()[0]
         if total_pledges is None:
             total_pledges = 0
+
         return total_pledges
+
+    @property
+    def num_pledges(self):
+        return self.pledges.count()
 
     @property
     def num_days_left(self):
@@ -59,17 +63,77 @@ class Project(db.Model):
         return num_days_left
 
     @property
+    def percentage_funded(self):
+        return int(self.total_pledges * 100 / self.goal_amount)
+
+    @property
     def image_path(self):
         return cloudinary.utils.cloudinary_url(self.image_filename)[0]
 
     @property
-    def percentage_funded(self):
-        return int(self.total_pledges * 100 / self.goal_amount)
+    def duration(self):
+        return (self.time_end - self.time_start).days
+
+    def get_num_pledges_datapoints(self):
+        pledges_per_day = db.session.query(
+            func.date(Pledge.time_created),
+            func.count(Pledge.time_created)
+        ).filter(
+            Project.id == self.id,
+            Project.id == Pledge.project_id
+        ).group_by(
+            func.date(Pledge.time_created)
+        ).all()
+
+        ret = [[i + 1, 0] for i in range(self.duration)]
+
+        for p in pledges_per_day:
+            time_pledged = datetime.datetime.strptime(p[0], '%Y-%m-%d')
+            day_num = (time_pledged.date() - self.time_start.date()).days + 1
+            num_pledges = p[1]
+            ret[day_num] = [day_num, num_pledges]
+
+        return ret
+
+    def get_amount_pledged_datapoints(self):
+        amount_per_day = db.session.query(
+            func.date(Pledge.time_created),
+            func.sum(Pledge.amount)
+        ).filter(
+            Project.id == self.id,
+            Project.id == Pledge.project_id
+        ).group_by(
+            func.date(Pledge.time_created)
+        ).all()
+
+        ret = [[i + 1, 0] for i in range(self.duration)]
+
+        for p in amount_per_day:
+            time_pledged = datetime.datetime.strptime(p[0], '%Y-%m-%d')
+            day_num = (time_pledged.date() - self.time_start.date()).days + 1
+            amount = p[1]
+            ret[day_num] = [day_num, amount]
+
+        return ret
 
 
 class Pledge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
-    amount = db.Column(db.Integer)
-    time_created = db.Column(db.DateTime)
+    reward_id = db.Column(db.Integer, db.ForeignKey('reward.id'), nullable=True)
+    amount = db.Column(db.Integer, nullable=False)
+    time_created = db.Column(db.DateTime(timezone=False), nullable=False)
+
+
+class Reward(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    title = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    minimum_pledge_amount = db.Column(db.Integer, nullable=False)
+    pledges = db.relationship('Pledge', backref='reward', lazy='dynamic', foreign_keys='Pledge.reward_id')
+
+    @property
+    def num_pledges(self):
+        return self.pledges.count()
